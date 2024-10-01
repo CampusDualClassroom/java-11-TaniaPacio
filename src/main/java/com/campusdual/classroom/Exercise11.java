60b7a3f05c97da3012b8fd2a88d4606d51960f53
Tania Pacio Rivas <tnabret@hotmail.com>
2024-10-01 14:49:59 +0200

    Exercicio11resolto

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -6,6 +6,37 @@ public class Exercise11 {
         TVRemote blackRemote = new TVRemote("Negro");
         System.out.println(redRemote.getColor());
         System.out.println(blackRemote.getColor());
+
+
+        TVRemote ejercicioTv = new TVRemote("Black");
+
+        ejercicioTv.turnOn();
+
+        ejercicioTv.channelUp();
+        System.out.println("Channel actual: " + ejercicioTv.channel);
+
+        ejercicioTv.channelDown();
+        System.out.println("Channel actual: " + ejercicioTv.channel);
+
+        ejercicioTv.channelDown();
+        System.out.println("Channel actual: " + ejercicioTv.channel);
+
+        ejercicioTv.volumeUp();
+        System.out.println("Volumen actual: " + ejercicioTv.volume);
+
+        ejercicioTv.volumeDown();
+        System.out.println("Volumen actual: " + ejercicioTv.volume);
+
+        while(ejercicioTv.volume > 0){
+            ejercicioTv.volumeDown();
+        }
+        ejercicioTv.volumeDown();
+        System.out.println("Volumen actual: " + ejercicioTv.volume);
+
+        System.out.println("Color actual: " + ejercicioTv.getColor());
+
+        ejercicioTv.turnOff();
+
     }
 
 }
